697dd4f32c527af2a62b364793264cdef446ed1e
Respowerr <50842024+respowerr@users.noreply.github.com>
2024-02-27 11:09:54 +0100

    Fix env var

--- db.js
+++ db.js
@@ -1,15 +1,12 @@
 const { Sequelize } = require('sequelize')
-const db_host = process.env.db_host;
-const db_name = process.env.db_name;
-const db_pass = process.env.db_pass;
-const db_user = process.env.db_user;
+
 // database
 const sequelize = new Sequelize(
-  db_name, // Database name
-  db_user, // User
-  db_pass, // Password
+  process.env.db_name, // Database name
+  process.env.db_user, // User
+  process.env.db_pass, // Password
   {
-    host: db_host, // Host
+    host: process.env.db_host, // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {

af366ca4c0b8c3736e0475304c09e9f3b5976ab1
Respowerr <50842024+respowerr@users.noreply.github.com>
2024-02-27 11:02:32 +0100

    Rechange

--- db.js
+++ db.js
@@ -1,5 +1,8 @@
 const { Sequelize } = require('sequelize')
-
+const db_host = process.env.db_host;
+const db_name = process.env.db_name;
+const db_pass = process.env.db_pass;
+const db_user = process.env.db_user;
 // database
 const sequelize = new Sequelize(
   db_name, // Database name
